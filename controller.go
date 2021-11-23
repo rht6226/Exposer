4bd870a49e90e64953abbd9af700d6a3f10cd472
Rohit Anand <rohitraazanand567@gmail.com>
2021-11-23 22:14:01 +0530

    Run Implementation

--- controller.go
+++ controller.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	applisters "k8s.io/client-go/listers/apps/v1"
@@ -9,19 +13,19 @@ import (
 )
 
 type controller struct {
-	clientset            kubernetes.Interface
-	deploymentLister     applisters.DeploymentLister
-	deplymentCacheSynced cache.InformerSynced
-	queue                workqueue.RateLimitingInterface
+	clientset             kubernetes.Interface
+	deploymentLister      applisters.DeploymentLister
+	deploymentCacheSynced cache.InformerSynced
+	queue                 workqueue.RateLimitingInterface
 }
 
 func NewController(clientset kubernetes.Interface,
 	depInformer appsinformers.DeploymentInformer) *controller {
 	c := &controller{
-		clientset:            clientset,
-		deploymentLister:     depInformer.Lister(),
-		deplymentCacheSynced: depInformer.Informer().HasSynced,
-		queue:                workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "expose"),
+		clientset:             clientset,
+		deploymentLister:      depInformer.Lister(),
+		deploymentCacheSynced: depInformer.Informer().HasSynced,
+		queue:                 workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "expose"),
 	}
 
 	depInformer.Informer().AddEventHandler(
@@ -32,4 +36,17 @@ func NewController(clientset kubernetes.Interface,
 	)
 
 	return c
+}
+
+func (c *controller) Run(ch <-chan struct{}) {
+	fmt.Println("[INFO]: Starting controller...")
+	if !cache.WaitForCacheSync(ch, c.deploymentCacheSynced) {
+		fmt.Println("[ERROR]: Waiting for chace to be synced ...")
+	}
+
+	wait.Until(c.Worker, 1*time.Second, ch)
+}
+
+func (c *controller) Worker() {
+	fmt.Println("[INFO]: Working ...")
 }
